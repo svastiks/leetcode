e315660dbe19ae9b0b0e22fc41fe96a17e7b11f8
svastik <svastiksharma13@gmail.com>
2025-04-22 22:41:01 -0400

    updated 448

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -2,7 +2,11 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
 
         """
-        Since the original array has duplicates we remove them using a set (set's do. not allow duplicates)
+        Since the original array has duplicates we remove them using a set (set's do. not allow duplicates).
+
+        We also check for sets since sets are implemented using hash tables which is way faster for lookups i.e. O(1) time 
+        whereas if we do `if i not in nums`, nums here is a List and it will go through the whole list each iteration which is terrible for
+        time complexity and will definetly exceed the time limit.
 
         Time Complexity: O(n)
         Space Complexity: O(n)
