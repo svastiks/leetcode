f0ae36543252a91f883b96fa534021214ea612eb
Svastik Sharma <84158891+svastiks@users.noreply.github.com>
2025-07-07 07:58:10 -0400

    solved 238_product_of_array_except_self.py

---
+++ 238_product_of_array_except_self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        left_products = [1] * n
+        right_products = [1] * n
+        
+        answer = [1] * n
+
+        for i in range(1, n):
+            left_products[i] = left_products[i-1] * nums[i-1]
+
+        for i in range(n - 2, -1, -1):
+            right_products[i] = right_products[i+1] * nums[i+1]
+            
+        for i in range(n):
+            answer[i] = left_products[i] * right_products[i]
+            
+        return answer
