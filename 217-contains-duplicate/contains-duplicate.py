0b8fd502d9a7614d0ce3df34b446426b145a38f4
svastik <svastiksharma13@gmail.com>
2025-07-09 09:50:24 -0400

    moved solution files to root

--- 217-contains-duplicate/contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        
-        setArray = set(nums)
-
-        if(len(setArray) == len(nums)):
-            return False
-        else:
-            return True
